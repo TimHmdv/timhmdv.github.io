1a8768a51b2a7857e16f80c2877e78a6324050dd
Timur Hamidov <dakota4126@gmail.com>
2022-07-07 16:05:08 +0400

    added carousels slick slider part

--- pulse/src/js/script.js
+++ pulse/src/js/script.js
@@ -1,3 +1,34 @@
 $(document).ready(function(){
-    $('.carousel-slider').slick();
+    $('.carousel__inner').slick(
+      {
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/slider/slider_arrow-left.png" alt="arrow-left"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/slider/slider_arrow-right.png" alt="arrow-right"></button>',
+        adaptiveHeight: true,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      }
+    );
   });
