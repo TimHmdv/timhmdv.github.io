ee24627889a2aa912a59be2318e6484dbc5c2fbc
Timur Hamidov <dakota4126@gmail.com>
2022-07-07 17:10:53 +0400

    slider arrows img path fix

--- pulse/src/js/script.js
+++ pulse/src/js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
     $('.carousel__inner').slick(
       {
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/slider/slider_arrow-left.png" alt="arrow-left"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/slider/slider_arrow-right.png" alt="arrow-right"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/slider/slider_arrow-left.png" alt="arrow-left"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/slider/slider_arrow-right.png" alt="arrow-right"></button>',
         adaptiveHeight: true,
         responsive: [
           {
