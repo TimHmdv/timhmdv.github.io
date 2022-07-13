d96a79ec615d979ce18052657915abd9c85e7de8
Timur Hamidov <dakota4126@gmail.com>
2022-07-13 18:26:56 +0400

    added part of catalog adaptation

--- pulse/src/js/script.js
+++ pulse/src/js/script.js
@@ -1,36 +1,48 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick(
-      {
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/slider/slider_arrow-left.png" alt="arrow-left"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/slider/slider_arrow-right.png" alt="arrow-right"></button>',
-        responsive: [
-          {
-            breakpoint: 992,
-            settings: {
-              dots: true,
-              arrows: false
-            }
-          }
-        ]
-      }
-    );
+$(document).ready(function () {
+	$('.carousel .slider').slick(
+		{
+			prevArrow: '<button type="button" class="slick-prev"><img src="icons/slider/slider_arrow-left.png" alt="arrow-left"></button>',
+			nextArrow: '<button type="button" class="slick-next"><img src="icons/slider/slider_arrow-right.png" alt="arrow-right"></button>',
+			responsive: [
+				{
+					breakpoint: 992,
+					settings: {
+						dots: true,
+						arrows: false
+					}
+				}
+			]
+		}
+	);
 
-    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
-      $(this)
-        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-        .closest('.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
-    });
+	$('.catalog .slider').slick(
+		{
+			prevArrow: '<button type="button" class="slick-prev"><img src="icons/slider/slider_arrow-left.png" alt="arrow-left"></button>',
+			nextArrow: '<button type="button" class="slick-next"><img src="icons/slider/slider_arrow-right.png" alt="arrow-right"></button>'
+		}
+	);
 
-    function toggleTabSlide (item) {
-      $(item).each(function (i) {
-        $(this).on('click', function(e) {
-          e.preventDefault();
-          $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-          $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-        })
-      });
-    }
+	$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+		$(this)
+			.addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+			.closest('.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+	});
 
-    toggleTabSlide('.catalog-item__link');
-    toggleTabSlide('.catalog-item__back');
-  });
+	$('.catalog .slider').on('click swipe', function () {
+		let currentSlide = $('.catalog .slider').slick('slickCurrentSlide');
+		$(this).closest('.container').find('div.catalog__content').removeClass('catalog__content_active').eq(currentSlide).addClass('catalog__content_active');
+	});
+
+	function toggleTabSlide(item) {
+		$(item).each(function (i) {
+			$(this).on('click', function (e) {
+				e.preventDefault();
+				$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+				$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+			})
+		});
+	}
+
+	toggleTabSlide('.catalog-item__link');
+	toggleTabSlide('.catalog-item__back');
+});
