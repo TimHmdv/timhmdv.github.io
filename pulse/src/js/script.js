c8e49482f6546d50d31f5d323c99a14460eff9e4
dakota96 <dakota4126@gmail.com>
2022-07-31 20:34:36 +0400

    added phpmailer to send data from forms

--- pulse/src/js/script.js
+++ pulse/src/js/script.js
@@ -111,4 +111,18 @@ $(document).ready(function () {
 
 	$('input[name="phone"]').mask("+7 (999) 999-99-99");
 
+	$('form').submit(function (e) {
+		e.preventDefault();
+		$.ajax({
+			type: "POST",
+			url: "mailer/smart.php",
+			data: $(this).serialize(),
+			success: function (response) {
+				$(this).find('input').val('');
+			
+				$('form').trigger('reset');
+			}
+		});
+		return false;
+	});
 });
