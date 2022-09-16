803f073db80d218677ace1f5856ae0d262740e6b
timhmdv <dakota4126@gmail.com>
2022-09-16 15:32:12 +0400

    added listener on DOMContentLoaded and wow.js initialization

--- portfolio/src/js/script.js
+++ portfolio/src/js/script.js
@@ -1,87 +1,91 @@
-// Hamburger and Menu listeners
-const hamburgerElement = document.querySelector('.hamburger'),
-      menuElement = document.querySelector('.menu'),
-      menuLinkElements = document.querySelectorAll('.menu__link'),
-      closeElement = document.querySelector('.menu__close'),
-      overlayElement = document.querySelector('.menu__overlay');
+window.addEventListener('DOMContentLoaded', () => {
+    // Hamburger and Menu listeners
+    const hamburgerElement = document.querySelector('.hamburger'),
+    menuElement = document.querySelector('.menu'),
+    menuLinkElements = document.querySelectorAll('.menu__link'),
+    closeElement = document.querySelector('.menu__close'),
+    overlayElement = document.querySelector('.menu__overlay');
 
-hamburgerElement.addEventListener('click', () => {
+    hamburgerElement.addEventListener('click', () => {
     menuElement.classList.add('active');
-});
-
-menuLinkElements.forEach(function (menuLinkElement) {
-    menuLinkElement.addEventListener('click', () => {
-        menuElement.classList.remove('active');
     });
-})
 
-closeElement.addEventListener('click', () => {
+    menuLinkElements.forEach(function (menuLinkElement) {
+    menuLinkElement.addEventListener('click', () => {
     menuElement.classList.remove('active');
-});
+    });
+    })
 
-overlayElement.addEventListener('click', () => {
+    closeElement.addEventListener('click', () => {
     menuElement.classList.remove('active');
-});
+    });
 
+    overlayElement.addEventListener('click', () => {
+    menuElement.classList.remove('active');
+    });
 
-// Bar level calculating
-const levelsValue = document.querySelectorAll('.skills__level-value');
-const barsValue = document.querySelectorAll('.skills__level-bar');
+    // Bar level calculating
+    const levelsValue = document.querySelectorAll('.skills__level-value');
+    const barsValue = document.querySelectorAll('.skills__level-bar');
 
-levelsValue.forEach(function(levelValue, index) {
+    levelsValue.forEach(function(levelValue, index) {
     barsValue[index].style.width = levelValue.innerHTML;
-});
+    });
 
-// Smooth Scrolling
-let links = document.querySelectorAll('[href^="#"]');
-const element = document.documentElement,
-body = document.body;
+    // Smooth Scrolling
+    let links = document.querySelectorAll('[href^="#"]');
+    const element = document.documentElement,
+    body = document.body;
 
-links.forEach(link => {
+    links.forEach(link => {
     link.addEventListener('click', function(event) {
-        let scrollTop = Math.round(body.scrollTop || element.scrollTop);
+    let scrollTop = Math.round(body.scrollTop || element.scrollTop);
 
-        if (this.hash !== '') {
-            event.preventDefault();
-            let hashElement = document.querySelector(this.hash),
-                hashElementTop = 0;
+    if (this.hash !== '') {
+        event.preventDefault();
+        let hashElement = document.querySelector(this.hash),
+            hashElementTop = 0;
 
-            while (hashElement.offsetParent) {
-                hashElementTop += hashElement.offsetTop;
-                hashElement = hashElement.offsetParent;
-            }
-
-            hashElementTop = Math.round(hashElementTop);
-            smoothScroll(scrollTop, hashElementTop, this.hash);
+        while (hashElement.offsetParent) {
+            hashElementTop += hashElement.offsetTop;
+            hashElement = hashElement.offsetParent;
         }
-    });  
-})
 
-const smoothScroll = (from, to, hash) => {
+        hashElementTop = Math.round(hashElementTop);
+        smoothScroll(scrollTop, hashElementTop, this.hash);
+    }
+    });  
+    })
+
+    const smoothScroll = (from, to, hash) => {
     let timeInterval = 1,
-        prevScrollTop,
-        speed;
+    prevScrollTop,
+    speed;
 
     if (to > from) {
-        speed = 30;
+    speed = 30;
     } else {
-        speed = -30;
+    speed = -30;
     }
-    
-    let move = setInterval(function() {
-        let scrollTop = Math.round(body.scrollTop || element.scrollTop);
 
-        if (
-            prevScrollTop === scrollTop ||
-            (to > from && scrollTop >= to) ||
-            (to < from && scrollTop <= to)
-        ) {
-            clearInterval(move);
-            history.replaceState(history.state, document.title, location.href.replace(/#.*$/g, '') + hash);
-        } else {
-            body.scrollTop += speed;
-            element.scrollTop += speed;
-            prevScrollTop = scrollTop;
-        }
+    let move = setInterval(function() {
+    let scrollTop = Math.round(body.scrollTop || element.scrollTop);
+
+    if (
+        prevScrollTop === scrollTop ||
+        (to > from && scrollTop >= to) ||
+        (to < from && scrollTop <= to)
+    ) {
+        clearInterval(move);
+        history.replaceState(history.state, document.title, location.href.replace(/#.*$/g, '') + hash);
+    } else {
+        body.scrollTop += speed;
+        element.scrollTop += speed;
+        prevScrollTop = scrollTop;
+    }
     }, timeInterval);
-};
+    };
+
+    // WOW.js library initialization
+    new WOW().init();
+})
