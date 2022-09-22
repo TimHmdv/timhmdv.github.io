e69ba0eb44e04edb3b9f2028eb98964adba44a21
timhmdv <dakota4126@gmail.com>
2022-09-22 17:38:28 +0400

    added server side captcha check

--- portfolio/src/mailer/smart.php
+++ portfolio/src/mailer/smart.php
@@ -36,10 +36,30 @@ $mail->Body    = '
 	E-mail: ' . $email . '	
 ';
 
-if(!$mail->send()) {
-    return false;
-} else {
-    return true;
+$response = $_POST["g-recaptcha-response"];
+$url = 'https://www.google.com/recaptcha/api/siteverify';
+$data = array(
+    'secret' => 'YOUR_SECRET',
+    'response' => $_POST["g-recaptcha-response"]
+);
+$options = array(
+    'http' => array (
+        'method' => 'POST',
+        'content' => http_build_query($data)
+    )
+);
+$context  = stream_context_create($options);
+$verify = file_get_contents($url, false, $context);
+$captcha_success=json_decode($verify);
+
+ if ($captcha_success->success==false) {
+    echo "Captcha wrong";
+} else if ($captcha_success->success==true) {
+	if(!$mail->send()) {
+		return false;
+	} else {
+		return true;
+	}
 }
 
 ?>
