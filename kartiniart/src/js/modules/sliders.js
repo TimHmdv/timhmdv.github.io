b55cb653fff5c1022345207c7ea65e61cd11e840
dakota96 <dakota4126@gmail.com>
2022-11-27 23:51:57 +0400

    added part of slider functionality

---
+++ kartiniart/src/js/modules/sliders.js
@@ -0,0 +1,45 @@
+const sliders = (slides, direction, previous, next) => {
+    let slideIndex = 1;
+    const items = document.querySelectorAll(slides);
+           
+    function showSlides(n) {
+        if (n > items.length) {
+            slideIndex = 1;
+        }
+
+        if (n < 1) {
+            slideIndex = items.length;
+        }
+
+        items.forEach(item => {
+            item.classList.add('animated');
+            item.style.display = 'none';
+        });
+
+        items[slideIndex - 1].style.display = 'block';
+    }
+
+    showSlides(slideIndex);
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    try {
+        const  prevBtn = document.querySelector(previous),
+                nextBtn = document.querySelector(next);
+        
+        prevBtn.addEventListener('click', () => {
+            plusSlides(-1);
+        })
+
+        nextBtn.addEventListener('click', () => {
+            plusSlides(1);
+        })
+
+    } catch (e) {
+
+    }
+}
+
+export default sliders;
